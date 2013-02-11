1c1af10255b60b422df19ddfc45e4d3ede1f6ba4
Sean Stangl <sstangl@mozilla.com>
2013-02-11 15:39:59 -0800

    Rename dumped directory to "js-1.8.8" from "mozilla-esr17".

--- package.sh
+++ package.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-REPODIR="$HOME/dev/mozilla-esr17"
+DIRNAME="js-1.8.8"
+REPONAME="mozilla-esr17"
+REPODIR="$HOME/dev/${REPONAME}"
 BUILDDIR=$(pwd)
 
 cd "$REPODIR"
 
 hg revert -a
 hg st -un | xargs rm
+
 
 PACKAGEVERSION=188-1.0.0
 PACKAGEFULLVERSION="${PACKAGEVERSION}~hg"$(date +%Y%m%d)".esr17."$(hg id -i | cut -c -8)
@@ -21,7 +24,15 @@ autoconf-2.13
 cd "$REPODIR"
 cd ..
 
-tar Jcf $BUILDDIR/js${PACKAGEVERSION}.tar.xz --exclude='.hg' --exclude="*.orig" --exclude="*.rej" mozilla-esr17/js/jsd mozilla-esr17/js/public mozilla-esr17/js/src mozilla-esr17/mfbt
+tar Jcf $BUILDDIR/js${PACKAGEVERSION}.tar.xz                                  \
+  --exclude='.hg'                                                             \
+  --exclude="*.orig"                                                          \
+  --exclude="*.rej"                                                           \
+  --transform s/${REPONAME}/${DIRNAME}/                                       \
+  ${REPONAME}/js/jsd                                                          \
+  ${REPONAME}/js/public                                                       \
+  ${REPONAME}/js/src                                                          \
+  ${REPONAME}/mfbt
 
 cd "$BUILDDIR"
 
