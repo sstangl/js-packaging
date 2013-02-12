29e79974eaad60f87403664ce556f6e051185ce7
Sean Stangl <sstangl@mozilla.com>
2013-02-11 17:23:32 -0800

    Include applied patches in tarball via "patches/". Quell some warnings.

--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ REPONAME="mozilla-esr17"
 REPODIR="$HOME/dev/${REPONAME}"
 BUILDDIR=$(pwd)
 
+
 cd "$REPODIR"
 
 hg revert -a
@@ -14,10 +15,11 @@ hg st -un | xargs rm
 PACKAGEVERSION=188-0.0.1
 PACKAGEFULLVERSION="${PACKAGEVERSION}~hg"$(date +%Y%m%d)".esr17."$(hg id -i | cut -c -8)
 
-patch -p1 < $BUILDDIR/moz188-libname-changes.patch
-patch -p1 < $BUILDDIR/moz188-fix-version.patch
-patch -p1 < $BUILDDIR/bug831552-install-headers.patch
-patch -p1 < $BUILDDIR/bug835551-required-defines.patch
+patch -p1 < $BUILDDIR/patches/moz188-libname-changes.patch
+patch -p1 < $BUILDDIR/patches/moz188-fix-version.patch
+patch -p1 < $BUILDDIR/patches/bug831552-install-headers.patch
+patch -p1 < $BUILDDIR/patches/bug835551-required-defines.patch
+patch -p1 < $BUILDDIR/patches/quell-common-warnings.patch
 
 cd js/src
 autoconf-2.13
@@ -25,9 +27,10 @@ autoconf-2.13
 cd "$REPODIR"
 cd ..
 
-tar Jcf $BUILDDIR/js${PACKAGEVERSION}.tar.xz                                  \
-  --exclude='.hg'                                                             \
-  --exclude='.git'                                                            \
+# Include files from mozilla repository.
+TARFILE="${BUILDDIR}/js${PACKAGEVERSION}.tar"
+tar cf ${TARFILE}                                                             \
+  --exclude-vcs                                                               \
   --exclude="*.orig"                                                          \
   --exclude="*.rej"                                                           \
   --transform s/${REPONAME}/${DIRNAME}/                                       \
@@ -38,3 +41,5 @@ tar Jcf $BUILDDIR/js${PACKAGEVERSION}.tar.xz                                  \
 
 cd "$BUILDDIR"
 
+tar rf ${TARFILE} --transform "s#^#${DIRNAME}/#" patches
+gzip ${TARFILE}
